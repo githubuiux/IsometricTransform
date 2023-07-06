a64f0a725cca93782c26ed1539cd6d0e9589529d
githubuiux <uiuxista@gmail.com>
2023-07-06 11:59:22 +0300

    Initial Commit

---
+++ IsometricTransform/IsometricTransformApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsometricTransformApp.swift
+//  IsometricTransform
+//
+//  Created by Levent on 6.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct IsometricTransformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
